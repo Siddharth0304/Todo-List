18fc64a71cb913c7fc01d49a49223552a0714ff9
Siddharth Kamlekar <98891124+Siddharth0304@users.noreply.github.com>
2023-12-28 12:41:51 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,45 @@
+let inp=document.querySelector("input");
+let btn=document.querySelector("button");
+let ul=document.querySelector("ul");
+btn.addEventListener("click",add);
+
+function add(){
+    if(inp.value!=""){
+        let div=document.createElement("div");
+        let del=document.createElement("button");
+        let item=document.createElement("li");
+        div.classList.add("todo");
+        del.classList.add("delete");
+        del.innerText="x";
+        item.innerText=inp.value;
+        ul.appendChild(div);
+        div.appendChild(item);
+        div.appendChild(del);
+        inp.value="";
+    }
+}
+//TO REMOVE ELEMENTS:
+
+ul.addEventListener("click",function(event){
+    // console.log(event.target);
+    // console.log(event.target.nodeName);
+    if(event.target.nodeName=="BUTTON"){
+        let par=event.target.parentElement;
+        par.remove();
+    }
+})
+
+ul.addEventListener('click',function(event){
+    if(event.target.nodeName=="LI"){
+        let li=event.target;
+        li.classList.add("new");
+    }
+})
+
+// let delbtns=document.querySelectorAll(".delete");
+// for(delb of delbtns){
+//     delb.addEventListener("click",function(){
+//         let par=this.parentElement;
+//         par.remove();
+//     });
+// }
